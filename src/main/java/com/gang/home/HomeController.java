85316e47ba992432ba9999cfe95cda756d2139cd
Hongsugyeong <olivia258@naver.com>
2023-02-25 12:42:19 +0900

    mediaPipe

--- src/main/java/com/gang/home/HomeController.java
+++ src/main/java/com/gang/home/HomeController.java
@@ -10,5 +10,10 @@ public class HomeController {
 	public String home() {
 		return "/posenet/posenet";
 	}
+	
+	@GetMapping("mediapipe")
+	public String media() {
+		return "/posenet/mediaPipe";
+	}
 
 }
