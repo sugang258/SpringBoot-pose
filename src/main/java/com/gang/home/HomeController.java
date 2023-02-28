2a0069980368df33cf6c598a67c42b9d466646c5
olrlobt <317tmdgjs@naver.com>
2023-02-28 18:16:12 +0900

    fix: ClassPath 경로 수정

--- src/main/java/com/gang/home/HomeController.java
+++ src/main/java/com/gang/home/HomeController.java
@@ -1,7 +1,6 @@
 package com.gang.home;
 
 
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -37,30 +36,30 @@ public class HomeController {
 	
 	@GetMapping("/kakaoPose")
 	public String kakaoPose() {
-		String REST_API_KEY = "59a842b4b24abc7f4692e19f097b3766";
-		
-		RestTemplate restTemplate = new RestTemplate();
-		HttpHeaders headers = new HttpHeaders();
-		
-		
-		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-		headers.set("Authorization", "KakaoAK " + REST_API_KEY);
-
-		MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-		map.add("image_url", "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2F20130405_20%2Fstpbear_1365157580205zwuRh_JPEG%2FIMG_0040.jpg&type=sc960_832");
-
-		HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
-
-		String url = "https://cv-api.kakaobrain.com/pose";
-		ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
-
-		System.out.println(response.getBody());
+//		String REST_API_KEY = "59a842b4b24abc7f4692e19f097b3766";
+//		
+//		RestTemplate restTemplate = new RestTemplate();
+//		HttpHeaders headers = new HttpHeaders();
+//		
+//		
+//		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+//		headers.set("Authorization", "KakaoAK " + REST_API_KEY);
+//
+//		MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+//		map.add("image_url", "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2F20130405_20%2Fstpbear_1365157580205zwuRh_JPEG%2FIMG_0040.jpg&type=sc960_832");
+//
+//		HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
+//
+//		String url = "https://cv-api.kakaobrain.com/pose";
+//		ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
+//
+//		System.out.println(response.getBody());
 		
 //		headers.setContentType(MediaType.MULTIPART_FORM_DATA);
 //		headers.set("Authorization", "KakaoAK " + REST_API_KEY);
 //
 //		MultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
-//		map.add("file", new FileSystemResource("resources/static/sample/sample.jpg"));
+//		map.add("file", new FileSystemResource("src/main/resources/static/sample/test3.jpg"));
 //
 //		HttpEntity<MultiValueMap<String, Object>> request = new HttpEntity<>(map, headers);
 //
